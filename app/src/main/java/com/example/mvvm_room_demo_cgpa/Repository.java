84973414eab3dc2b1f14d82b6fd84ab5bdd11252
Ras <rasrif10008@gmail.com>
2020-11-04 09:39:09 +0600

    Seventh Commit from home

--- app/src/main/java/com/example/mvvm_room_demo_cgpa/Repository.java
+++ app/src/main/java/com/example/mvvm_room_demo_cgpa/Repository.java
@@ -1,4 +1,22 @@
 package com.example.mvvm_room_demo_cgpa;
 
+import android.app.Application;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+
 public class Repository {
+    public SemisterDao semisterDao;
+    LiveData<List<Semister>> allSemister;
+
+    Repository(Application application){
+        SemisterDatabase semisterDatabase = SemisterDatabase.getDatabase(application);
+        semisterDao = semisterDatabase.semisterDao();
+        allSemister = semisterDao.getAllSemister();
+    }
+
+    public LiveData<List<Semister>> getAllSemister() {
+        return allSemister;
+    }
 }
